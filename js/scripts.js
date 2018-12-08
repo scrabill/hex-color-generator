443e3d191a97c17c56924ac531a0a86472f14e5b
scrabill <shannon.crabill@gmail.com>
2018-12-08 13:47:57 -0500

    Adds const for length of hexidecimal integers array

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,5 @@ Output Hex value as text
 */
 
 const hexadecimalIntegers = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"];
+
+const lengthOfArray = hexadecimalIntegers.length; // Get the lenth of hexadecimalIntegers array
