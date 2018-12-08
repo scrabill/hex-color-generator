c3e974bb9e568e33381d6e809e64e1a1e330af40
scrabill <shannon.crabill@gmail.com>
2018-12-08 13:51:19 -0500

    Adds const for random number

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,5 @@ Output Hex value as text
 const hexadecimalIntegers = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"];
 
 const lengthOfArray = hexadecimalIntegers.length; // Get the lenth of hexadecimalIntegers array
+
+const randomNumber = Math.floor(Math.random() * lengthOfArray); // Randomly pick a number, no larger than the length of hexadecimalIntegers
