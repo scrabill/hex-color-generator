69e69f43a872b60ed2f62de4d195b956a676dc79
scrabill <shannon.crabill@gmail.com>
2019-01-13 14:24:48 -0500

    Adds createHexString function Creates a createHexString function, that gets the result of the createColor function. The result passed correctly. The next step is to run createColor 6 times, and pass each unique result into a new string

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,8 @@ Output Hex value as text
 
 */
 
+// const hexColor = null;
+
 const hexadecimalIntegers = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"];
 
 const lengthOfArray = hexadecimalIntegers.length; // Get the lenth of hexadecimalIntegers array
@@ -23,6 +25,38 @@ function createColor() {
   console.log("The background-color is: " + hexColor);
   // let body = document.querySelector("body"); // Target the body element
   // body.setAttribute('style', 'background-color: ' + hexColor);
+  return hexColor;
 }
 
-createColor();
+// createColor();
+
+function createHexString() {
+  let hexColor = createColor(); // Passes the result of createColor() into createHexString()
+
+  let hexString = "";
+
+  let hexStringLength = hexColor.length;
+
+  let i = 0;
+
+  console.log("The hexStringLength is: " + hexStringLength);
+
+  //How to trigger createColor() 6 unique times and add to a new string
+
+  while (i < 6) {
+    console.log("The string is:" + hexStringLength)
+
+    let hexString = hexColor;
+
+    i++;
+
+    console.log(hexString);
+
+    return hexString;
+
+
+   }
+
+}
+
+createHexString();
