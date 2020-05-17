76a89c0ea5a44b0003f70e2fcaf1a5f6ebe94e73
scrabill <shannon.crabill@gmail.com>
2020-05-17 13:25:40 -0400

    Removes unused text and variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
 let body = document.querySelector("body")
-let currentColor = ''
 
 generateRandomHexColor = () => {
   const hexadecimalIntegers = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"];
