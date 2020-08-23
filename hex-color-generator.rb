eed8f4c47846fdb073c562a56c1784b32b4c4c43
scrabill <shannon.crabill@gmail.com>
2020-08-22 22:00:41 -0400

    Delete ruby file

--- hex-color-generator.rb
+++
@@ -1,5 +0,0 @@
-def createColor
-  hexadecimalIntegers = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"]
-  color = hexadecimalIntegers.sample(6).join("")
-  "Your new hex color is ##{color}"
-end
